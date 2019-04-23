98058b4b294e1ee31f5c3d3cd2c3b9b11e0b7cc8
Svendsen92 <ssven16@student.aau.dk>
2019-04-23 14:54:01 +0200

    moved getRotationAngle() to geometricalData lib

--- include/geometricalData.h
+++ include/geometricalData.h
@@ -8,16 +8,17 @@
 class geometricalData
 {
     private:
-	   	double getMedian(std::vector<double> vec);
+	   	double getMedian_(std::vector<double> vec);
 	   	float getSquareArea_(cv::Point2f p1, cv::Point2f p2, cv::Point2f p3);
-	   	bool inObject(cv::Point2f *corner, cv::Point2f point);
+	   	bool inObject_(cv::Point2f *corner, cv::Point2f point);
 
 	   	std::vector<double> vecdist_;
 	   	int pixelRatio_ = 0;
 	   	double H_ = 0;
 	   	double pixelLength_ = 0;
 	   	double confDist_ = 0;
-	   	int numOfPixels_ = 92928; //img.cols * img.rows;
+	   	const float PI_ = 3.14159265359;
+	   	const int numOfPixels_ = 92928; //img.cols * img.rows;
 		const float A_ = -2467.8, B_ = 1.6624, C_ = 0.7498;
 
     public:
@@ -27,6 +28,7 @@ class geometricalData
         void setPixelLength(cv::Mat &xyz, cv::Mat &src, cv::Point2f *corners, double conf_dist);
 	   	double* getLengthAndWidth(double *LW, cv::Point2f *corners, double pixelLength);
 	   	double* getDimensions(cv::Mat &xyz, cv::Mat &src, double *LWH, cv::Point2f *corners, double conf_dist);
+	   	double getOrientation(cv::Point2f *corners);
 
 };
 
