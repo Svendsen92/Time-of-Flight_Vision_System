fe8a1f453c296e45aa3b9ae4d9bfe1611fe033c0
Svendsen92 <ssven16@student.aau.dk>
2019-04-24 23:19:06 +0200

    added logger library

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
 #include <memory>
+#include <thread>
+#include <signal.h>
+#include <chrono>  // for high_resolution_clock
+
 #include <opencv2/opencv.hpp>
 //#include <opencv2/imgproc/imgproc.hpp>
 #include <ifm3d/camera.h>
 #include <ifm3d/fg.h>
 #include <ifm3d/image.h>
 
-
-#include <thread>
-#include <signal.h>
-#include <chrono>  // for high_resolution_clock
-
-
 #include "config.h"
 #include "imgProcess.h"
 #include "geometricalData.h"
 #include "imgConstruct.h"
+#include "tcp_ip.h"
+#include "logger.h"
 
 
 struct thread_args {
-  	int theta;
+  	double theta, length, width, height;
   	cv::Mat src, xyz;
 
   	// locks for the different threads
-  	pthread_mutex_t img_mutex, theta_mutex, dimensions_mutex;
-  	pthread_cond_t  img_cond, theta_cond, dimensions_cond;
+  	pthread_mutex_t img_mutex, theta_mutex, dimensions_mutex, thetaTrans_mutex, dimensionsTrans_mutex;
+  	pthread_cond_t  img_cond, theta_cond, dimensions_cond, thetaTrans_cond, dimensionsTrans_cond;
 
   	cv::Point2f objCorners[4];
 };
 
+void* structInit(void* a) {
 
-void* structInit(void* a){
-
-	std::cerr << "structInit()" << std::endl;
+	std::cout << "structInit()" << std::endl;
 	thread_args *b;
 	b = (thread_args*)a;
 
@@ -44,31 +43,37 @@ void* structInit(void* a){
 	try
 	{
 		b->theta = 0;
+		b->length = 0;
+		b->width = 0;
+		b->height = 0;
 
-	  	b->img_mutex 		= PTHREAD_MUTEX_INITIALIZER;
-	  	b->img_cond 		= PTHREAD_COND_INITIALIZER;
-	  	b->theta_mutex 		= PTHREAD_MUTEX_INITIALIZER;
-	  	b->theta_cond 		= PTHREAD_COND_INITIALIZER;
-	  	b->dimensions_mutex = PTHREAD_MUTEX_INITIALIZER;
-	  	b->dimensions_cond 	= PTHREAD_COND_INITIALIZER; 	
-
+	  	b->img_mutex 				= PTHREAD_MUTEX_INITIALIZER;
+	  	b->img_cond 				= PTHREAD_COND_INITIALIZER;
+	  	b->theta_mutex 				= PTHREAD_MUTEX_INITIALIZER;
+	  	b->theta_cond 				= PTHREAD_COND_INITIALIZER;
+	  	b->dimensions_mutex 		= PTHREAD_MUTEX_INITIALIZER;
+	  	b->dimensions_cond 			= PTHREAD_COND_INITIALIZER;
+	  	b->thetaTrans_mutex 		= PTHREAD_MUTEX_INITIALIZER;
+	  	b->thetaTrans_cond			= PTHREAD_COND_INITIALIZER;
+	  	b->dimensionsTrans_mutex 	= PTHREAD_MUTEX_INITIALIZER;
+	  	b->dimensionsTrans_cond 	= PTHREAD_COND_INITIALIZER;
 	}
 	catch(const char* msg)
 	{
-		std::cerr << "ERROR... failed to initialize Struct variables!!" << std::endl;
+		std::cout << "ERROR... failed to initialize Struct variables!!" << std::endl;
 		exit(-1);
 	}
 }
 
+void* getImage(void* a) { 
 
-void* getImage(void* a){ 
-
-	std::cerr << "getImage()" << std::endl;
+	std::cout << "getImage()" << std::endl;
 
 	thread_args *b;
 	b = (thread_args*)a;
 
 	config c;
+	logger log("log-file");
 	imgConstruct construct;
 
 	auto cam = ifm3d::Camera::MakeShared();
@@ -85,13 +90,20 @@ void* getImage(void* a){
 	double roi_height = std::atof(c.getParameter("roi_y").c_str());
 	double conf_dist = std::atof(c.getParameter("dist").c_str());
 
+	log.write('m', "coeff_A: " + std::to_string(coeff[0]));
+	log.write('m', "coeff_B: " + std::to_string(coeff[1]));
+	log.write('m', "roi_width: " + std::to_string(roi_width));
+	log.write('m', "roi_height: " + std::to_string(roi_height));
+	log.write('m', "conf_dist: " + std::to_string(conf_dist));
+
 	while (true){
 
 		//auto get_start = std::chrono::high_resolution_clock::now();
 
 		if (! fg->WaitForFrame(img.get(), 1000))
 		{
-	      	std::cerr << "Timeout waiting for camera!" << std::endl;
+			log.write('e', "In getImage()... Timeout waiting for camera!");
+	      	std::cout << "Timeout waiting for camera!" << std::endl;
 	      	exit(-1);
 	    }
 
@@ -116,10 +128,9 @@ void* getImage(void* a){
 	}
 }
 
+void* pointCloudMethod(void* a) {
 
-void* pointCloudMethod(void* a){
-
-	std::cerr << "pointCloudMethod()" << std::endl;
+	std::cout << "pointCloudMethod()" << std::endl;
 
 	thread_args *b;
 	b = (thread_args*)a;
@@ -193,10 +204,6 @@ void* pointCloudMethod(void* a){
 		b->objCorners[1] = corners[1];
 		b->objCorners[2] = corners[2];
 		b->objCorners[3] = corners[3];
-
-
-	    //b->theta = getRotateAngle(corners);
-	    //std::cout << "theta: " << b->theta << std::endl;
 		
 	    pthread_mutex_unlock(&b->theta_mutex);
 	    pthread_mutex_unlock(&b->dimensions_mutex);
@@ -207,7 +214,6 @@ void* pointCloudMethod(void* a){
 	    //printf("image processing ms: %d\n", std::chrono::duration_cast<std::chrono::milliseconds>(process_finish - process_start).count());
 	}
 }
-
 
 void* getRotateAngle(void* a) {
 
@@ -225,13 +231,14 @@ void* getRotateAngle(void* a) {
 		//auto process_start = std::chrono::high_resolution_clock::now();
 		
 		b->theta = geoData.getOrientation(b->objCorners);
-		std::cout << "theta: " << b->theta << std::endl; 
+		std::cout << "theta: " << int(b->theta) << std::endl; 
+
+		pthread_cond_signal(&b->thetaTrans_cond);
 
 		//auto process_finish = std::chrono::high_resolution_clock::now();
 	    //printf("get rotation ms: %d\n", std::chrono::duration_cast<std::chrono::milliseconds>(process_finish - process_start).count());
 	}
 }
-
 
 void* getDimensions(void* a) {
 
@@ -256,24 +263,78 @@ void* getDimensions(void* a) {
 		double LWH[3];
 		double* L_W_H = geoData.getDimensions(b->xyz, b->src, LWH, b->objCorners, conf_dist);
 
+		b->length = L_W_H[0];
+		b->width = L_W_H[1];
+		b->height = L_W_H[2];
+
+		pthread_cond_signal(&b->dimensionsTrans_cond);
+
 		//auto dim_finish = std::chrono::high_resolution_clock::now();
 	    //printf("get dimensions ms: %d\n", std::chrono::duration_cast<std::chrono::milliseconds>(dim_finish - dim_start).count());
 
 
-		std::cout << "\nobject length: " << std::setprecision(3) << L_W_H[0]/10 << "cm" << std::endl;
-		std::cout << "object width: " << std::setprecision(3) << L_W_H[1]/10 << "cm" << std::endl;		
-		std::cout << "object height: " << std::setprecision(3) << L_W_H[2]/10 << "cm" << std::endl;
+		std::cout << "\nobject length: " << std::setprecision(3) << b->length / 10 << "cm" << std::endl;
+		std::cout << "object width: " << std::setprecision(3) << b->width / 10 << "cm" << std::endl;		
+		std::cout << "object height: " << std::setprecision(3) << b->height / 10 << "cm" << std::endl;
 	}
 }
 
+void* dataTransfer(void* a) {
 
+	std::cerr << "sendData()" << std::endl;
+
+	thread_args *b;
+	b = (thread_args*)a;
+
+	logger log("log-file");
+
+	tcp_ip t;
+	int port = 12345;
+	bool tcpFailure = true;
+
+	while (true)
+	{
+		pthread_cond_wait(&b->thetaTrans_cond, &b->thetaTrans_mutex);
+		pthread_cond_wait(&b->dimensionsTrans_cond, &b->dimensionsTrans_mutex);
+
+		//auto dim_start = std::chrono::high_resolution_clock::now();
+
+		std::string data;
+		data = "theta: ";
+		data += std::to_string(b->theta) + ", ";
+		data += "length: ";
+		data += std::to_string(b->length) + ", ";
+		data += "width: ";
+		data += std::to_string(b->width) + ", ";
+		data += "height: ";
+		data += std::to_string(b->height);
+
+		while (tcpFailure) 
+		{
+			t.sendData(data, port);
+
+			if (t.tcpSuccess)
+			{
+				tcpFailure = false;
+				std::cout << "data has been sent" << std::endl;
+			}
+			else
+			{
+				log.write('e', "In dataTransfer()... failed to send the data");
+				std::cout << "failed to send the data" << std::endl;	
+			}
+		}			
+
+		//auto dim_finish = std::chrono::high_resolution_clock::now();
+	    //printf("get dimensions ms: %d\n", std::chrono::duration_cast<std::chrono::milliseconds>(dim_finish - dim_start).count());
+	}
+}
 
 
 
 int main(int argc, const char **argv){
 
 	thread_args *args = new thread_args;
-
 
   	// Initializes the variables in the Struct
   	structInit((void*) args);
@@ -287,12 +348,15 @@ int main(int argc, const char **argv){
 	sleep(2);
 	std::thread thread4(getRotateAngle, args);
 	sleep(2);
+	//std::thread thread5(dataTransfer, args);
+	//sleep(2);
 
 	// Holds the main thread until the thread has finished
 	thread1.join();
   	thread2.join();
   	thread3.join();
   	thread4.join();
+  	//thread5.join();
 
   	return 0;
 }
